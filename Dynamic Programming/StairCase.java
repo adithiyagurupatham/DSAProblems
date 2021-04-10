e684e1c67f36e2f10ee7e41d5debc69663f8cfe2
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-04-11 01:09:58 +0530

    possible number of ways to reach nth staircase

---
+++ Dynamic Programming/StairCase.java
@@ -0,0 +1,27 @@
+/*
+   Find the maximum possible number of ways to reach nth staircase if we can move the staircase by 1 step or 2 step at a time
+*/
+
+import java.util.*;
+import java.io.*;
+public class StairCase{
+  public static void main(String[] args){
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Integer input = Integer.valueOf(br.readLine());
+    System.out.println(getPossibleWays(input));
+  }
+
+  static int getPossibleWays(Integer input){
+    if(input<=2){
+      return input;
+    }
+    int[] possibleWays = new int[input+1];
+    possibleWays[1]=1;
+    possibleWays[2]=2;
+    for(int i=3;i<=n;i++){
+      possibleWays[i] = possibleWays[i-1]+possibleWays[i-2];
+    }
+    return possibleWays[n];
+  }
+
+}
