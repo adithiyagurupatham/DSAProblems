415b391d4707c2200c842c20a84fa228f3a7497a
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-04-17 13:00:27 +0530

    pair count with duplicates

---
+++ Arrays/CountPairSumWithDuplicates.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+public class CountPairSumWithDuplicates{
+
+  static int getPairCountWithDuplicates(int[] array,int sum){
+    int counter =0;
+    Map<Integer,Integer> hashMap = new HashMap<>();
+    for(int each : array){
+      if(hashMap.containsKey(each)){
+        hashMap.put(each,hashMap.get(each)+1);
+      }
+      else{
+        hashMap.put(each,1);
+      }
+    }
+    for(int each : array){
+      if(hashMap.containsKey(sum-each)){
+        counter +=hashMap.get(sum-each);
+        if(sum-each == each){
+          counter--;
+        }
+      }
+    }
+    return counter/2;
+  }
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the input size");
+    Integer size = Integer.valueOf(br.readLine());
+    int[] array = new int[size];
+    System.out.println("Enter the elements");
+    String[] inputString = br.readLine().split(" ");
+    for(int i=0;i<size;i++){
+      array[i] = Integer.valueOf(inputString[i]);
+    }
+    System.out.println("Enter the sum");
+    Integer sum = Integer.valueOf(br.readLine());
+    System.out.println("The pair count is "+getPairCountWithDuplicates(array,sum));
+  }
+}
