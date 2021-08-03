649ca691d08557c08bbd24a79a5562c211f1b370
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-08-03 23:50:28 +0530

    last occurance of element in sorted array

---
+++ Searching/SortedLastOccuranceNormal.java
@@ -0,0 +1,35 @@
+/**
+Program to find the last index of the given value in sorted array
+Time Complexity : O(N)
+Space Complexity : O(1)
+*/
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class SortedLastOccuranceNormal{
+
+  static int findValue(int[] array,int val){
+    int ans=-1;
+    for(int i=0;i<array.length;i++){
+      if(array[i]==val){
+        ans=i;
+      }
+    }
+    return ans;
+  }
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the input size");
+    Integer size = Integer.valueOf(br.readLine());
+    int[] array = new int[size];
+    System.out.println("Enter the elements");
+    String[] inputString = br.readLine().split(" ");
+    for(int i=0;i<size;i++){
+      array[i] = Integer.valueOf(inputString[i]);
+    }
+    System.out.println("Enter the value to be searched");
+    Integer value = Integer.valueOf(br.readLine());
+    System.out.println("The index of the first occured value is "+findValue(array,value));
+  }
+}
