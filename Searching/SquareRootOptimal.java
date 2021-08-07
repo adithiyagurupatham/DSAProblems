59a37207ee4224be090e124f7b60706ac2053d90
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-08-07 23:22:12 +0530

    square root optimal approach

--- Searching/SquareRootOptimal.java
+++ Searching/SquareRootOptimal.java
@@ -1,18 +1,29 @@
 /**
 Program to find the floor squareRoot of a given number
-Time Complexity : O(Sqrt(N))
+Time Complexity : O(logN)
 Space Complexity : O(1)
 */
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-public class SquareRootNormal{
+public class SquareRootOptimal{
   static int getSquareRoot(int number){
-    int i=1;
-    while(i*i<=number){
-      i++;
+    int low = 1,high = number,ans=-1;
+    while(low<=high){
+      int mid = low + ((high - low)/2);
+      int midSqr = mid*mid;
+      if(midSqr==number){
+        return mid;
+      }
+      if(midSqr<=number){
+        ans = mid;
+        low = mid+1;
+      }
+      else{
+        high = mid - 1;
+      }
     }
-    return i-1;
+    return ans;
   }
 
   public static void main(String[] args) throws IOException{
