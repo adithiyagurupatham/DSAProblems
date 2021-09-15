cdd82aeba1edb2b2db436c173cc59d9d0cad1f85
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-09-16 02:49:23 +0530

    two arrays union optimal approach

--- Hashing/Two arrays Union/TwoArraysUnionOptimal.java
+++ Hashing/Two arrays Union/TwoArraysUnionOptimal.java
@@ -1,35 +1,28 @@
 /**
 Program to find the union count of two arrays
 Link : https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1/
-Time Complexity : O(m^2+(n*(m*n)))
+Time Complexity : O(m+n)
 Space Complexity : O(1)
 */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.Map;
-public class TwoArraysUnionNormal{
+import java.util.Set;
+public class TwoArraysUnionOptimal{
 
   static int doUnion(int a[], int n, int b[], int m)
     {
-        ArrayList<Integer> temp = new ArrayList<>();
-        int ans=0;
-        for(int each : a){
-            if(!temp.contains(each)){
-                ans++;
-                temp.add(each);
-            }
-        }
-        for(int each : b){
-            if(!temp.contains(each)){
-                ans++;
-                temp.add(each);
-            }
-        }
-        return ans;
+      Set<Integer> ans = new HashSet<>();
+      for(int each : a){
+          ans.add(each);
+      }
+      for(int each : b){
+          ans.add(each);
+      }
+      return ans.size();
 
     }
 
@@ -52,7 +45,7 @@ public class TwoArraysUnionNormal{
       array1[i] = Integer.valueOf(stringArray[i]);
     }
     System.out.println("The total number of union elements are "
-          +doUnion(array,array1,size,size1));
+          +doUnion(array,size,array1,size1));
   }
 
 }
