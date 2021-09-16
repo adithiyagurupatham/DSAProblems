7e8bcb0c159c57271a5e897d494ca8bd34bda8fc
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-09-16 23:29:51 +0530

    zero sum subarray existence check

---
+++ Hashing/ZeroSubArraySumNormal.java
@@ -0,0 +1,43 @@
+/**
+Program to check if subarray sum equals zero or not
+Time Complexity : O(N^2)
+Space Complexity : O(1)
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+public class ZeroSubArraySumNormal{
+
+  static boolean zeroSubArraySumExist(int arr[], int n) {
+      for(int i=0;i<n;i++){
+        int currSum=0;
+        for(int j=i;j<n;j++){
+            currSum+=arr[j];
+            if(currSum==0){
+              return true;
+            }
+        }
+      }
+      return false;
+    }
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the number of elements");
+    Integer size = Integer.valueOf(br.readLine());
+    int[] array = new int[size];
+    System.out.println("Enter the elements");
+    String[] stringArray = br.readLine().split(" ");
+    for(int i=0;i<stringArray.length;i++){
+      array[i] = Integer.valueOf(stringArray[i]);
+    }
+    System.out.println("The zero subarray sum existence status is "
+          +zeroSubArraySumExist(array, size));
+
+  }
+
+}
