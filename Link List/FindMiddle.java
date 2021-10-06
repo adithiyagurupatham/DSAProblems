42b615a382ea7985d261ced03c25dfd95c0da5a8
Adithiya Guru Patham <adithiyagurupatham@gmail.com>
2021-10-07 03:45:37 +0530

    find the middle element of link list

---
+++ Link List/FindMiddle.java
@@ -0,0 +1,64 @@
+/**
+Program to find the middle element of the link list
+Link : https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+Time Complexity : O(N)
+Space Complexity : O(1)
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+class FindMiddle
+{
+  private static class Node{
+     Integer data;
+     Node next;
+    Node(int data)
+    {
+      this.data = data;
+    }
+  }
+
+  static void print(Node head)
+  {
+    Node temp = head;
+    while(temp!=null)
+    {
+      System.out.print(temp.data);
+      temp = temp.next;
+      if(temp!=null)
+      {
+        System.out.print("->");
+      }
+    }
+  }
+
+  static  int getMiddle(Node head)
+    {
+         Node slow =head,fast = head;
+         while(fast!=null && fast.next!=null)
+         {
+             slow = slow.next;
+             fast = fast.next.next;
+         }
+         return slow.data;
+    }
+
+
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter the count of values");
+    Integer n = Integer.valueOf(br.readLine());
+    System.out.println("Enter the values");
+    int val = Integer.valueOf(br.readLine());
+    Node head = new Node(val);
+    Node temp = head;
+    for(int i=0;i<n-1;i++)
+    {
+      val = Integer.valueOf(br.readLine());
+      Node node = new Node(val);
+      temp.next = node;
+      temp = temp.next;
+    }
+    System.out.println("The middle element is "+getMiddle(head));
+  }
+}
